f51757c1d2d50296b3c9cd2c4e306f141c8abb2f
yokeshr21 <yokeshr21@gmail.com>
2026-02-23 12:30:33 +0530

    Add Maven wrapper, git configuration, and test setup

---
+++ src/test/java/com/undoschool/course_search/CourseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undoschool.course_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
