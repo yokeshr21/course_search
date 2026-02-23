db7987205acfc0f351ecc6613e4afeb9e98e09bb
yokeshr21 <yokeshr21@gmail.com>
2026-02-23 12:09:50 +0530

    Initial Spring Boot project setup with base structure

---
+++ src/main/java/com/undoschool/course_search/CourseSearchApplication.java
@@ -0,0 +1,13 @@
+package com.undoschool.course_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSearchApplication.class, args);
+	}
+
+}
